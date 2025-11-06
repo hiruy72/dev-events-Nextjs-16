b8cd93891eadd9409a349d08d0ea831aa627a24f
Hiruy Legesse <hiruyadane@gmail.com>
2025-11-06 13:09:11 -0800

    add some animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Explorebtn from "@/components/Explorebtn";
 
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <section>
+            <h1 className="text-center">
+                The Hub for Every Dev <br/> Event You can't Miss
+            </h1>
+            <p className="text-center mt-5 ">
+                Hackathons, Meetups, and Conferences , All in One Place
+            </p>
+            <Explorebtn/>
+        </section>
     )
 }
 export default Page
